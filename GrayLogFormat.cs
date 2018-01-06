c6c591147d06ba36151b48fabdbe85412f4fdbe0
vdasus <me@vdasus.com>
2018-01-06 13:36:53 +0200

    read from option from settings

--- GrayLogFormat.cs
+++ GrayLogFormat.cs
@@ -12,23 +12,31 @@ namespace GrayLog
     /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "GrayLog")]
-    [Name("GrayLogText")]
+    [Name("GrayLog")]
     [UserVisible(true)]
     [Order(After = Priority.High)]
     internal sealed class GrayLogFormat : ClassificationFormatDefinition
     {
         public GrayLogFormat()
         {
+            /*Color clr = Color.FromRgb(84, 112, 109);*/
+            Color clr = Color.FromRgb(84, 0, 0);
+
             try
             {
                 //https://stackoverflow.com/questions/6641899/how-to-encapsulate-user-setting-options-page-in-visual-studio-2010-addin
                 //https://msdn.microsoft.com/en-us/library/bb166195.aspx
                 DTE dte = Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE)) as DTE;
                 // Access options page
-                Properties props = dte?.get_Properties("GrayLog", "General");
+                Properties props = dte?.Properties["GrayLog", "General"];
                 
-                var pathProperty = props?.Item("OptionColor");
-                Color path = (Color) pathProperty?.Value;
+                Property pathProperty = props?.Item("OptionColor");
+                if (pathProperty?.Value != null)
+                {
+                    clr = UIntToColor((uint)pathProperty?.Value);
+                }
+
+                /*GrayLogToolsOptionsPackage myToolsOptionsPackage = this.package as GrayLogToolsOptionsPackage;*/
             }
             catch (Exception ex)
             {
@@ -37,10 +45,17 @@ namespace GrayLog
             
             DisplayName = "GrayLog Text"; //human readable version of the name
             BackgroundOpacity = 0;
-            
-            ForegroundColor = Color.FromRgb(84, 112, 109);
-            //ForegroundColor = Color.FromRgb(115, 132, 84);
+            ForegroundColor = clr;
             //BackgroundColor = Colors.Orange;
+        }
+
+        private static Color UIntToColor(uint color)
+        {
+            var a = (byte)(color >> 24);
+            var r = (byte)(color >> 16);
+            var g = (byte)(color >> 8);
+            var b = (byte)(color >> 0);
+            return Color.FromArgb(a, r, g, b);
         }
     }
 }
--- GrayLogToolsOptionsPackage.cs
+++ GrayLogToolsOptionsPackage.cs
@@ -1,16 +1,38 @@
 ﻿using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
+using System.Drawing;
 
 namespace GrayLog
 {
     /*[Guid(GuidList.guidMyToolsOptionsPkgString)]*/
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    /*[ProvideProfile(typeof(OptionGrayLog),
+        "GrayLog", "General", 106, 107, isToolsOptionPage: true, DescriptionResourceID = 108)]*/
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(OptionGrayLog), "GrayLog", "General", 0, 0, true)]
     [Guid(PACKAGE_GUID_STRING)]
     public sealed class GrayLogToolsOptionsPackage : Package
     {
+        /*public const string PACKAGE_GUID_STRING = "2511FF91-C7ED-4406-A28C-165386B64398";*/
         public const string PACKAGE_GUID_STRING = "2511FF91-C7ED-4406-A28C-165327B64398";
+    
+        public bool OptionBool
+        {
+            get
+            {
+                OptionGrayLog page = (OptionGrayLog)GetDialogPage(typeof(OptionGrayLog));
+                return page.OptionBool;
+            }
+        }
+
+        public Color OptionColor
+        {
+            get
+            {
+                OptionGrayLog page = (OptionGrayLog)GetDialogPage(typeof(OptionGrayLog));
+                return page.OptionColor;
+            }
+        }
     }
 }
